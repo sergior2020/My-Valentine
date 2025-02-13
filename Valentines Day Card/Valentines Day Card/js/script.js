6e49c6e6a6e0be9000ed7484942626a927a7253e
sergior2020 <srodriguez2020@fau.edu>
2025-02-13 13:07:48 -0500

    Add files via upload

---
+++ Valentines Day Card/Valentines Day Card/js/script.js
@@ -0,0 +1,8 @@
+function openCard() {
+    document.querySelector('.card').classList.add('open');
+}
+function closeCard(event) {
+    event.stopPropagation();
+    document.querySelector('.card').classList.remove('open');
+
+}
